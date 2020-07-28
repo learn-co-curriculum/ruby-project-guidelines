6d700ff00b4bb96255122fbee707e1d29a9ea07e
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-07-28 15:30:33 -0500

    purchasing

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,5 @@
 require_relative '../config/environment'
 
-#puts "HELLO WORLD"
-
 def welcome_user
     puts "*******************************"
     puts "       Welcome To Our          "
@@ -14,12 +12,24 @@ def welcome_user
      puts "Sign-in with your Username."
      gets.chomp
  end
+
+ def item_input
+    gets.chomp
+ end
+
+ def choose_input
+    gets.chomp
+ end
  
  #-----Runs our code----->
  def run
      welcome_user
      $username = user_input
      create_new_user_if_not_exist
+     $item_name=item_input
+     search_for
+     you_want_it?
+     $choose_op=choose_input
  end
 
  #------Method lets us know if user exists or not------>
@@ -29,22 +39,40 @@ def welcome_user
         namelist.push(m.name)
     end
     if namelist.include? $username
-            puts "Welcome back."
+            puts "Welcome back. What are you looking for?"
     else
         newuser=User.create(name: $username)
         puts "What are you looking for?"
     end
  end
 
+ #---show the list of available Items--->
+ def search_for
+    Item.search_by_name($item_name)  
+ end
+
+ #---Choose item--->
+ def you_want_it?
+    puts "Is there anything you like? Y/N"
+    if $choose_op == Y
+        make_a_purchase(item2)
+    else
+        puts "Aonther search"
+    end
+end
+
  #---Making purchase-->
- def make_a_purchase(username)
-    newpurchase= Purchase.new(username.id,item2.id)
+ def make_a_purchase
+    newpurchase= Purchase.new($username .id,item2.id)
+    User.total($username )
+ end
+
+#---Make/Write a Review-->
+ def write_a_review
+
  end
  
 run
+#--We can do-->
 
-
-
-# Item.search_by_name('Dell')   
-# User.total("Yehong")
 # Review.see_my_review("Yordin")
