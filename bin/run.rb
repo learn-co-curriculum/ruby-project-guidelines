2ba0ef4a700f3a6c0115d80144d5183357ee0d7f
tincogs <tomoday91@gmail.com>
2020-09-30 17:12:50 -0500

    User choice methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,19 +1,23 @@
 require_relative '../config/environment'
 require_relative '../lib/user_interface/command_line_interface.rb'
 
+new_cli = CommandLineInterface.new
 
-welcome
+new_cli.welcome
 
-username = username_input
+username = new_cli.username_input
 
-# account = find_or_create_by_name(username)
+account = new_cli.find_or_create_by_name(username)
 
-menu_item = get_food_order
+menu_item = new_cli.get_food_order
 #show_restaurants_and_prices(menu_item)
 
+item_id = new_cli.user_choice_id(menu_item)
 
+options = new_cli.restaurant_menu_item_matches(item_id)
 
-
+puts options
+#last_call
 
 # def show_restaurants_and_prices(menu_item) #returns input menu_item
 #     puts menu_item
--- lib/user_interface/command_line_interface.rb
+++ lib/user_interface/command_line_interface.rb
@@ -1,34 +1,55 @@
 require "tty-prompt"
 
-def welcome
-    puts "The Dinner Bell's Ringing! Let's Eat!"
-end
+class CommandLineInterface
 
-def username_input
-    puts "Please enter a username:"
-    username = STDIN.gets.chomp.downcase
-end
-
-def find_or_create_by_name(username)
-    user = User.find_or_create_by(name: username)
-    puts "Welcome to Let's Eat #{user.name}!"
-end
-
-def get_food_order
-    puts "What do you have a taste for?"
-    STDIN.gets.chomp.downcase #takes in the user's food request & downcases it.
-    last_call #creates a loop for those who wish to change their food choice
-end
-
-def last_call
-    puts "Are you sure that's what you want? (y/n)" #prompts the user to make a food choice
-    options = STDIN.gets.chomp.downcase #takes a response from the user
-    if options == "y" #if they say y, yes, print message below
-        puts "Thank you for using Let's Eat!"
-    elsif options == "n" #if they say n, no, loop back to getting the food order
-        get_food_order #take another food order
-    else #if they put anything besides y or n prints message below and loops this method
-       puts "Sorry, that was not a valid answer. Please press y for yes or n for no."
-       last_call #loop this method
+    def welcome
+        puts "The Dinner Bell's Ringing! Let's Eat!"
     end
+
+    def username_input
+        puts "Please enter a username:"
+        username = STDIN.gets.chomp.downcase
+    end
+
+    def find_or_create_by_name(username)
+        user = User.find_or_create_by(name: username)
+        puts "Welcome to Let's Eat #{user.name}!"
+    end
+
+    def get_food_order
+        prompt = TTY::Prompt.new
+        prompt.select("What do you have a taste for?", MenuItem.all.map{|item| item.name})
+        # STDIN.gets.chomp.downcase #takes in the user's food request & downcases it.
+        #last_call #creates a loop for those who wish to change their food choice
+    end
+
+    def last_call
+        prompt = TTY::Prompt.new
+        choice = prompt.select("Are you sure that's what you want?", ["Yes","No"])
+        if choice == "No"
+            get_food_order
+        else
+            puts "Thank you for using Let's Eat!"
+        end
+        # puts "Are you sure that's what you want? (y/n)" #prompts the user to make a food choice
+        # options = STDIN.gets.chomp.downcase #takes a response from the user
+        # if options == "y" #if they say y, yes, print message below
+        #     puts "Thank you for using Let's Eat!"
+        # elsif options == "n" #if they say n, no, loop back to getting the food order
+        #     get_food_order #take another food order
+        # else #if they put anything besides y or n prints message below and loops this method
+        #    puts "Sorry, that was not a valid answer. Please press y for yes or n for no."
+        #    last_call #loop this method
+        # end
+    end
+
+    def user_choice_id(choice)
+        user_choice = MenuItem.find_by(name: choice)
+        user_choice.id
+    end
+
+    def restaurant_menu_item_matches(menu_input)
+        matches = RestaurantMenuItem.where(menu_item_id: menu_input)
+    end
+
 end
