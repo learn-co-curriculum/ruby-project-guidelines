76830e4a4327d5e393ed6855f4d68e79b4833a02
aarora16 <a.99.anshul@gmail.com>
2020-10-21 17:00:51 -0500

    tried to configure seeds but there is still a bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+# require_relative '../app/models/event.rb'
+
+ActiveRecord::Base.establish_connection
 
 data = RestClient.get "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=222&apikey=6lzMfDxzzO4DBnjAU7OfrYkAlMBPcS5f"
 parsed_data = JSON.parse(data)
@@ -23,8 +26,28 @@ def add_info_to_events(all_events)
   end
 end
 
-def add_info_to_venues
-  
+def add_info_to_venues(all_events)
+  all_events.each do |event|
+    new_venue = {}
+    venue = event["_embedded"]["venues"][0]
+
+    new_venue["name"] = venue["name"]
+    new_venue["url"] = venue["url"]
+    new_venue["state"] = venue["state"]["name"]
+    new_venue["city"] = venue["city"]["name"]
+    new_venue["postal_code"] = venue["postalCode"]
+
+    @venues << new_venue
+  end
 end
 
 add_info_to_events(all_events)
+add_info_to_venues(all_events)
+
+@events.each do |event|
+  Event.create(event)
+end
+
+@venues.each do |venue|
+  Venue.create(venue)
+end
