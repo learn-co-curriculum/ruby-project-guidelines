9eec381823753d7cb8b46a866b28b32106c39042
aarora16 <a.99.anshul@gmail.com>
2020-10-22 14:25:21 -0500

    added functionality to update preferences

--- app/cli/cli.rb
+++ app/cli/cli.rb
@@ -41,12 +41,130 @@ def help_menu
   when "Venues Menu"
     venues_menu()
   when "Sign out"
-    run()
+    exit
   end
 end
 
 def view_preferences
+  user_prefs = @current_user.attributes
+  3.times {user_prefs.shift}
+
+  if user_prefs.values.uniq.include? nil
+    puts "Looks like all your preferences haven't been set yet"
+    updating_preferences()
+  else
+    puts "• Genre: #{@current_user.genre}"
+    puts "• Minimum Price: #{@current_user.min_price}"
+    puts "• Maximum Price: #{@current_user.max_price}"
+    puts "• State: #{@current_user.state}"
+    puts "• City: #{@current_user.city}"
+    puts "• Postal Code: #{@current_user.postal_code}"
+  end
+end
+
+def updating_preferences
+  prompt = TTY::Prompt.new
+  input = prompt.yes?("Would you like to update them now?")
+  if input
+    update_preferences()
+  else 
+    help_menu()
+  end 
+end
+
+def update_preferences
+  prompt = TTY::Prompt.new
+  input = prompt.select("Which preferences would you like to update?") do |menu|
+    menu.choice "Genre"
+    menu.choice "Minimum Price"
+    menu.choice "Maximum Price"
+    menu.choice "State"
+    menu.choice "City"
+    menu.choice "Postal Code"
+  end
   
+  case input
+  when "Genre"
+    update_genre()
+  when "Minimum Price"
+    update_min_price()
+  when "Maximum Price"
+    update_max_price()
+  when "State"
+    update_state()
+  when "City"
+    update_city()
+  when "Postal Code"
+    update_postal_code()
+  end
+end
+
+def update_genre
+  genres = Event.all.map do |event|
+    event.genre
+  end
+
+  genres.uniq!
+
+  prompt = TTY::Prompt.new
+  input = prompt.select("Which genre do you prefer?", genres)
+  @current_user.genre = input
+  @current_user.save
+end
+
+def update_min_price
+  prompt = TTY::Prompt.new
+  input = prompt.ask("Please enter a minimum price:")
+
+  @current_user.min_price = input.to_f
+  @current_user.save
+end
+
+def update_max_price
+  prompt = TTY::Prompt.new
+  input = prompt.ask("Please enter a maximum price:")
+
+  @current_user.max_price = input.to_f
+  @current_user.save
+end
+
+def update_city
+  cities = Venue.all.map do |venue|
+    venue.city
+  end
+
+  cities.uniq!
+
+  prompt = TTY::Prompt.new
+  input = prompt.select("Which city do you want to see concerts in?", cities)
+  @current_user.city = input
+  @current_user.save
+end
+
+def update_state
+  states = Venue.all.map do |venue|
+    venue.state
+  end
+
+  states.uniq!
+
+  prompt = TTY::Prompt.new
+  input = prompt.select("Which state do you want to see concerts in?", states)
+  @current_user.state = input
+  @current_user.save
+end
+
+def update_postal_code
+  postals = Venue.all.map do |venue|
+    venue.postal_code
+  end
+
+  postals.uniq!
+
+  prompt = TTY::Prompt.new
+  input = prompt.select("Which postal code do you want to see concerts in?", postals)
+  @current_user.postal_code = input
+  @current_user.save
 end
 
 def valid_user?(username)
