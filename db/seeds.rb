7ffcb392c496ed661a2a0a4b3b1cc6c5f8052655
dszarkiel <d.szarkiel@gmail.com>
2020-11-11 13:31:21 -0600

    0131PM

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Owner.destroy_all
 Tank.destroy_all
-TankOwnerIds.destroy_all
+TankOwnerId.destroy_all
 Fish.destroy_all
 
 bart = Owner.create(name: "bart")
@@ -13,11 +13,11 @@ tank2 = Tank.create(name: "tank2", fish_limit: 5)
 tank3 =Tank.create(name: "tank3", fish_limit: 5)
 tank4 =Tank.create(name: "tank4", fish_limit: 5)
 
-tank_owner_id_1 = TankOwnerIds.create(owner_id: bart.id, tank_id: tank1.id)
-tank_owner_id_2 = TankOwnerIds.create(owner_id: dom.id, tank_id: tank2.id)
-tank_owner_id_3 = TankOwnerIds.create(owner_id: max.id, tank_id: tank3.id)
-tank_owner_id_4 = TankOwnerIds.create(owner_id: derick.id, tank_id: tank4.id)
-tank_owner_id_5 = TankOwnerIds.create(owner_id: dom.id, tank_id: tank1.id)
+tank_owner_id_1 = TankOwnerId.create(owner_id: bart.id, tank_id: tank1.id)
+tank_owner_id_2 = TankOwnerId.create(owner_id: dom.id, tank_id: tank2.id)
+tank_owner_id_3 = TankOwnerId.create(owner_id: max.id, tank_id: tank3.id)
+tank_owner_id_4 = TankOwnerId.create(owner_id: derick.id, tank_id: tank4.id)
+tank_owner_id_5 = TankOwnerId.create(owner_id: dom.id, tank_id: tank1.id)
 
 nemo = Fish.create(name: "nemo", color: "orange", tank_id: tank1.id)
 dory = Fish.create(name: "dory", color: "blue", tank_id: tank2.id)
