49139cc56efd382cb3d00a3d187f1c363f24468f
shreya-sridhar <shreyasridhar1109@gmail.com>
2020-12-02 20:55:33 +0000

    added model relationships

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,4 @@
 class Food < ActiveRecord::Base
+    belongs_to: customer
+    belongs_to: order
 end 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ActiveRecord::Base
+    has_many :foods
+    has_many :customers, through: :foods  
 end 
