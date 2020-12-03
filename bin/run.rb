1835c766fb417c9203ca155470dedf5e5160af84
dwisecar <dwisecar@gmail.com>
2020-12-03 10:13:30 -0800

    created load_to_db

--- bin/run.rb
+++ bin/run.rb
@@ -12,7 +12,7 @@ def search_by_city
     puts "enter a city"
     city = gets
     info = GetRequester.new("https://app.ticketmaster.com/discovery/v2/events.json?city=#{city}&apikey=QATrioQ3vEzlLyBebumHRHuNBfT39vrZ").parse_json
-    puts event_details(info)
+    load_event_details(info)
 end
 
 def events_count
@@ -28,6 +28,23 @@ def event_details(info)
         i+=1
     end
     events
+end
+
+def load_event_details(info)   
+    events
+    info["_embedded"]["events"].each do |event|        
+        new_event = Event.new
+        new_event.attraction_name = event["name"]
+        new_event.date = event["dates"]["start"]["localDate"]
+        new_event.venue = event["_embedded"]["venues"][0]["name"]
+        new_event.genre = event["classifications"][0]["subGenre"]["name"]
+        event << new_event
+    end
+    events
+end
+
+def save_new_events
+
 end
 
 search_by_city
