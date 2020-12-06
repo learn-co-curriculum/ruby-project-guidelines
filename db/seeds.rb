1b4b31777c55ff5adcefe863f2f35da193bb8ab2
handsomemolly <ryan.kendig@gmail.com>
2020-12-06 13:58:27 -0800

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Match.destroy_all
 Arena.destroy_all
-Team.desteroy_all
+Team.destroy_all
 
 t1 = Team.new(name: "Liverpool")
 t2 = Team.new(name: "Manchester City")
@@ -8,4 +8,7 @@ t2 = Team.new(name: "Manchester City")
 a1 = Arena.new(name: "First Arena")
 a2 = Arena.new(name: "Second Arena")
 
-m1 = Match.new (date: "#{Time.now}", home_team_id: t1.id, away_team_id: t2.id, home_team_goals: 3, away_team_goals: 2, arena_id: a1.id)
+m1 = Match.new(date: "#{Time.now}", home_team_id: t1.id, away_team_id: t2.id, home_team_goals: 3, away_team_goals: 2, arena_id: a1.id)
+
+
+puts "Bingo!"
