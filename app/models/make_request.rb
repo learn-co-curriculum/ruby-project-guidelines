d874b657e45736aded92a4135a571714059f31ec
handsomemolly <ryan.kendig@gmail.com>
2020-12-06 18:44:25 -0800

    populating matches without IDs

--- app/models/make_request.rb
+++ app/models/make_request.rb
@@ -13,53 +13,61 @@ class MakeRequest < ActiveRecord::Base
     URI("https://api-football-v1.p.rapidapi.com/v2/fixtures/league/2?timezone=Europe%2FLondon") #fixtures in Premier League
 ]
 @@http = []
-urls.each do |url|
+@@urls.each do |url|
     h = Net::HTTP.new(url.host, url.port)
     h.use_ssl = true
     h.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http.push(h)
+    @@http.push(h)
 end
 
 @@requests = []
-urls.each do |url|
+@@urls.each do |url|
     r = Net::HTTP::Get.new(url)
     r["x-rapidapi-key"] = '680adbf113mshff91628eec25474p194702jsnd1c76f97e364'
     r["x-rapidapi-host"] = 'api-football-v1.p.rapidapi.com'
-    requests.push(r)
+    @@requests.push(r)
 end
 
 @@responses = []
 count = 0
-http.each do |h|
-    responses.push(h.request(requests[count]))
+@@http.each do |h|
+    @@responses.push(h.request(@@requests[count]))
     count += 1
 end
 
+def self.find_team_id_by_name(name)
+    Team.find_by(name: name)
+end
+
+def self.find_arena_id_by_name(name)
+    Arena.find_by(name: name)
+end
 
 
 def self.populate_match
-    match = JSON.parse(responses[1].read_body)
+    match = JSON.parse(@@responses[1].read_body)
     match_filtered = match['api']['fixtures'].map{|m|  [m['event_date'], m['homeTeam']['team_name'], m['awayTeam']['team_name'], m['goalsHomeTeam'], m['goalsAwayTeam'], m['venue']]}
+    
     match_filtered.each do |m|
-        Match.create(date: m[0], home_team_id: Team.find_by(name:"#{m[1]}").id, away_team_id: Team.find_by(name:"#{m[2]}").id, home_team_goals: m[3], away_team_goals: m[4], arena_id: Arena.find_by(name: "#{m[5]}").id)
+        Match.create(date: m[0], home_team_id: self.find_team_id_by_name(m[1]), away_team_id: self.find_team_id_by_name(m[2]), home_team_goals: m[3], away_team_goals: m[4], arena_id: self.find_arena_id_by_name(m[5]))
     end
 end
 
 def self.populate_team
-    team = JSON.parse(responses[0].read_body)
+    team = JSON.parse(@@responses[0].read_body)
     team_filtered = team['api']['teams'].map{|m| m['name']}
     team_filtered.each do |t|
-        Team.create(name: t[0])
+        Team.create(name: t)
     end
 end
 
 
 
 def self.populate_arena
-    team = JSON.parse(responses[0].read_body)
+    team = JSON.parse(@@responses[0].read_body)
     arena = team['api']['teams'].map{|m| m['venue_name']}
     arena.each do |a|
-        Arena.create(name: a[0])
+        Arena.create(name: a)
     end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ t2 = Team.create(name: "Manchester City")
 a1 = Arena.create(name: "First Arena")
 a2 = Arena.create(name: "Second Arena")
 
-m1 = Match.create(date: "#{Time.now}", home_team_id: t1.id, away_team_id: t2.id, home_team_goals: 3, away_team_goals: 2, arena_id: a1.id)
+# m1 = Match.create(date: "#{Time.now}", home_team_id: t1.id, away_team_id: t2.id, home_team_goals: 3, away_team_goals: 2, arena_id: a1.id)
 
 
 puts "Bingo!"
