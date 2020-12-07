0c61983fc8a79288e2830be8a673e201eeb19d8e
dangnhon <dang.nhon12345@yahoo.com>
2020-12-07 11:07:53 -0800

    please delete comment in artist line 6

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,3 +2,5 @@ class Artist < ActiveRecord::Base
     has_many :show_dates
     has_many :venues, through: :show_dates 
 end
+
+#This is to test our git branch push and pulls. please delete if you can pull this, Hunter. 
