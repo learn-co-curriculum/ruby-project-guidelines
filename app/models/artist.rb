525f613e25f71bcabef0851f1b270933da55a0a3
Hunter Posey <ihposey@crimson.ua.edu>
2020-12-07 11:17:58 -0800

    Deleted Comment on artist class, line 6

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,3 @@ class Artist < ActiveRecord::Base
     has_many :show_dates
     has_many :venues, through: :show_dates 
 end
-
-#This is to test our git branch push and pulls. please delete if you can pull this, Hunter. 
