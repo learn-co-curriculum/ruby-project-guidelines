50e50404f301484b69030e64660ae5807eac982c
Matt <mattfsewell@gmail.com>
2021-01-27 14:53:08 -0500

    newest

--- bin/api_parsing.rb
+++ bin/api_parsing.rb
@@ -10,7 +10,6 @@ def get_data(name)
     url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{name}"
     response = RestClient.get(url)
     result = JSON.parse(response)
-    
     while result["drinks"] == nil
         puts "Drink not found. Please input another recipe name."
         name = gets.chomp
@@ -23,7 +22,7 @@ def get_data(name)
     drink_instructions = result["drinks"][0]["strInstructions"]
     current_ingr_num = 1
     current_recipe = Recipe.find_or_create_recipe(drink_name, drink_instructions)
-    
+
     while result["drinks"][0]["strIngredient#{current_ingr_num}"]
         current_ingredient = Ingredient.add__or_create_ingredient(result["drinks"][0]["strIngredient#{current_ingr_num}"])
         IngredientRecipe.find_or_create_ingredient_recipe(current_recipe.id, current_ingredient.id)
