3248c0e414ce33b99a62b4ca12fffa62335a05a8
github username <github email address>
2021-03-09 17:33:34 -0600

    small

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-t1=Theater.create(name: "donovan")
-=======
-m1 = Movie.create(title: "ddd", genre: "drama", showtime: 6, theater: "aaa")
 
->>>>>>> f95df87a034e71bbfa90751e78d0d823119441dd
+t1=Theater.create(name: "donovan")
+
+m1 = Movie.create(title: "ddd", genre: "drama", showtime: 6, theater: "aaa")
