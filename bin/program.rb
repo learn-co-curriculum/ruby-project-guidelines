e34a7b18a4f3e6f26b9681fc5515fe9955f90673
hadi <hadi_960@hotmail.com>
2021-03-10 16:43:27 -0600

    adds login

--- bin/program.rb
+++ bin/program.rb
@@ -22,60 +22,39 @@ def search
         end
     end
 
+def login
+    guests = Guest.all.map {|guest| guest.name}
+    prompt = TTY::Prompt.new
+    login_choice = prompt.select("What would you like to do?", ["Login", "Sign Up", "continue as a guest"])
+    if (login_choice == "Login")
+        current_username = prompt.ask("What is your username?")
+        current_password = prompt.ask("What is your password?")
+        if guests.include?(current_username) && Guest.all.find_by(password: current_password)
+            puts "Enjoy you movie!"
+        else 
+            puts "Incorrect username or password"
+            exit!
+        end
+    end
+end
 
-    
-    # ticket_purchase = Ticket.create(theater_id: commando.id , movie_id: m11.id, guest_id: g5.id)
-    # ticket_purchase
-
-
-        
-    
-
-
-
-
-
-#####Deliverables for Program#####
-#have a login prompt
-#be able to save info 
-# def title 
-#     fancy schmancy title 
+# def signup
+#     guests = Guest.all.map {|guest| guest.name}
+#     prompt = TTY::Prompt.new
+#     signup_choice = prompt.select("create username?", ["Login", "Sign Up", "continue as a guest"])
+#     if (signup_choice == "Signup")
+#         current_username = prompt.ask("What is your username?")
+#         current_password = prompt.ask("What is your password?")
+#         if guests.include?(current_username) && Guest.all.find_by(password: current_password)
+#             puts "Enjoy you movie!"
+#         else 
+#             puts "Incorrect username or password"
+#             exit!
+#         end
+#     end
 # end
 
-# def login 
-#     kdajfdkljfadlkjfads
-# end
 
-# def search
-#     where they say if they wanna view all movies or by genre
-# end
-
-# def movie choice 
-#     pick a showtime 
-# end
-
-# def purchase
-#     purchase movie choice at showtime choice
-# end
-
-# def confirmation
-#     would you like to cancel 
-# end
-
-# def rating 
-#     ask to leave rating
-# end
-
-# def end goodbye
-#     thanks for purchasing . see you at showtime
-# end
-
-# END
-
-
-
-
-
-
-
-
+login
+welcome
+search
