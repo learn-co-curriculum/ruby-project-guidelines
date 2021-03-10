97a51474cb839d45201e071088a8c305058d7a7b
hadi <hadi_960@hotmail.com>
2021-03-10 11:10:45 -0600

    stashing

--- apps/models/guest.rb
+++ apps/models/guest.rb
@@ -1,3 +1,3 @@
 class Guest < ActiveRecord::Base
-    has_many :ticket
+    has_many :tickets
 end
--- db/migrate/20210309224256_create_movies_table.rb
+++ db/migrate/20210309224256_create_movies_table.rb
@@ -1,9 +1,9 @@
 class CreateMoviesTable < ActiveRecord::Migration[6.1]
   def change
   create_table :movies do |t|
-  t.string :title
-  t.string :genre
-  t.integer :showtime
+  t.string :title_id
+  t.string :genre_id
+  t.integer :showtime_id
   t.string :theater
   end
   end
