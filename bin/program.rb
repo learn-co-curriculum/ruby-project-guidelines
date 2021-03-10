f1c70846ef66616ed0b6a81e408c433b447710c9
github username <github email address>
2021-03-10 17:28:25 -0600

    setting up more program

--- bin/program.rb
+++ bin/program.rb
@@ -7,21 +7,6 @@ puts "Welcome to"
 puts title.asciify("Command Theater")
 end
 
-def search
-    movies = Movie.all.map {|movie| movie.title}
-    prompt = TTY::Prompt.new
-    movie_choice = prompt.select("What would you like to see?", movies)
-    select_movie = Movie.find_by(title: movie_choice)
-    puts "You selected #{movie_choice}!!! We love that one"
-    menu_options = prompt.select("What would you like to do next", ["Purchase Ticket", "Start Over"])
-        if menu_options == "Purchase Ticket"
-            puts "Here is your ticket"
-            your_ticket = Ticket.create(theater_id: Theater.all.first.id, movie_id: select_movie.id, guest_id: Guest.all.first.id) 
-        else 
-            welcome
-        end
-    end
-
 def login
     guests = Guest.all.map {|guest| guest.name}
     prompt = TTY::Prompt.new
@@ -37,6 +22,40 @@ def login
         end
     end
 end
+
+def search
+    movies = Movie.all.map {|movie| movie.title}
+    prompt = TTY::Prompt.new
+    movie_choice = prompt.select("What would you like to see?", movies)
+    select_movie = Movie.find_by(title: movie_choice)
+    puts "You selected #{movie_choice}!!! We love that one!"
+    menu_options = prompt.select("What would you like to do next", ["Purchase Ticket", "Start Over"])
+        if menu_options == "Purchase Ticket"
+            puts "Here is your ticket! Excited yet?!"
+            your_ticket = Ticket.create(theater_id: Theater.all.first.id, movie_id: select_movie.id, guest_id: Guest.all.first.id) 
+            puts your_ticket
+            puts "You're going to see #{your_ticket.movie.title} at #{your_ticket.theater.name}. "
+        else 
+            welcome
+        end
+    end
+
+    def thanks
+        puts "Thank you for your purchase! Commando Theater is b.y.o.b so feel free and WELCOME to bring your own refreshments. We cant wait to watch a movie with you!"
+        prompt = TTY::Prompt.new
+        thanks_choice = prompt.select("What would you like to do?", ["Rate Your Commando Expereince", "Start Over"])
+            if thanks_choice == "Rate Your Commando Experience"
+                rate= prompt.select("Give us a rating...", ["1", "2", "3", "4", "5"])
+                if rate == "1"
+                    puts "Thank you for your feedback"
+                end
+            end
+        end
+                
+
+
+
+
 
 # def signup
 #     guests = Guest.all.map {|guest| guest.name}
@@ -55,6 +74,7 @@ end
 # end
 
 
-login
 welcome
+login
 search
+thanks
--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,5 @@ Bundler.require
 
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.logger = nil
 require_all 'apps'
