32b5f293dc9289ac7aa495b783b52c42ed8cf712
fairyaddict0211 <nyashaperrry2005@yahoo.com>
2021-03-10 15:53:37 -0500

    changes

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -20,15 +20,32 @@ class Lesson < ActiveRecord::Base
 
     def self.schedule_lesson(student)
         puts "What would you like to learn?"
-        answer = STDIN.gets.chomp 
-        tutors = Tutor.where(subject: answer) 
-        if tutors.exists?
-            tutors.each do |tutor| 
-             puts tutor.name
-             #Allow student to select a tutor (Toni or Eric) and schedule lesson
-             #Output lesson confirmation with puts statement.
-            end  
-        end
+            answer = STDIN.gets.chomp 
+        puts "When would you like to have your lesson?"
+            date = STDIN.gets.chomp
+        puts "Please enter your username."
+            name = STDIN.gets.chomp
+        puts "Who would you like to learn with?"
+            puts Tutor.all
+            tutor = STDIN.gets.chomp
+            # tutors = Tutor.where(subject: answer) 
+            #     if tutors.exists?
+            #         tutors.each do |tutor| 
+            #             puts tutor.name
+            #         end
+            #         puts "Please select a tutor."                   
+            #         response = STDIN.gets.chomp 
+            #         Tutor.find_by(name: response)
+                    Lesson.create(topic: answer, date: date, student: name, tutor: tutor)
+                    # if response == tutors.name
+                    #     return "Your lesson has been scheduled." 
+                 
+       
+                    #   
+            #         puts "Your lesson has been scheduled."
+            #  #Allow student to select a tutor (Toni or Eric) and schedule lesson
+            #  #Output lesson confirmation with puts statement.
+               
     end
 
     def self.past_lessons(student)
@@ -40,6 +57,10 @@ class Lesson < ActiveRecord::Base
                 puts lesson.tutor.name
             end
         end
+    end
+
+    def self.scheduled_lessons(student)
+
     end
 
 end
