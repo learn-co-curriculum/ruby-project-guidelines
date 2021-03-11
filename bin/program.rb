c98a82eb71033996bc90db1f366de9922371ff5c
github username <github email address>
2021-03-11 14:48:56 -0600

    search by genre

--- bin/program.rb
+++ bin/program.rb
@@ -1,89 +1,86 @@
 require 'pry'
 require_relative '../config/environment'
 
-
 def welcome 
 title=Artii::Base.new(:font=> "cursive")
 puts "Welcome to"
 puts title.asciify("Command Theater")
 end
 
+
 def login
-    guests = Guest.all.map {|guest| guest.name}
     prompt = TTY::Prompt.new
-    login_choice = prompt.select("What would you like to do?", ["Login", "Sign Up", "continue as a guest"])
-    if (login_choice == "Login")
-        current_username = prompt.ask("What is your username?")
-        current_password = prompt.ask("What is your password?")
-        if guests.include?(current_username) && Guest.all.find_by(password: current_password)
-            puts "Enjoy you movie!"
-        else 
-            puts "Incorrect username or password"
-            exit!
+    login_choice = prompt.select("What would you like to do?", ["Signup"])
+        if (login_choice == "Signup")
+            prompt = TTY::Prompt.new
+                new_username = prompt.ask('Create your username')
+                new_password = prompt.ask("What is your password?")
+                $guest= Guest.create(name: new_username, password: new_password)
+        end
+    end
+
+
+def search
+    movies = Movie.all.map {|movie| movie.title}
+    genres = Genre.all.map {|genre| genre.genre}
+    prompt = TTY::Prompt.new
+    search_choice = prompt.select("What would you like to see?", ["All Movies", "Genres"])
+    if (search_choice == "All Movies")
+        prompt = TTY::Prompt.new
+        choice = prompt.select("Pick a movie", movies)
+        puts "You selected #{choice}!!! We love that one!"
+        select_movie = Movie.find_by(title: choice)
+    
+        menu_options = prompt.select("What would you like to do next", ["Purchase Ticket", "Start Over"])
+        if menu_options == "Purchase Ticket"
+            puts "Here is your ticket! Excited yet?!"
+            your_ticket = Ticket.create(theater_id: Theater.all.first.id, movie_id: select_movie.id, guest_id: $guest.id) 
+            puts your_ticket
+            puts "Please save this id for your confirmation number." 
+            puts "You're going to see the #{select_movie.genre.genre} movie, #{your_ticket.movie.title}, @ #{select_movie.showtime} "
+        end
+        
+    elsif (search_choice == "Genres")
+            prompt = TTY::Prompt.new
+            genre_choice = prompt.select("Pick a genre", genres)
+            puts "You selected #{genre_choice}. That's a popular one!"
+            select_genre = Genre.find_by(genre: genre_choice)
+             genre_titles = select_genre.movies.map {|movie| movie.title}
+            prompt = TTY::Prompt.new
+            genre_movie_choice = prompt.select("Which movie?", genre_titles)
+            ticket_choice = Movie.find_by(title: genre_movie_choice)
+           
+    
+            prompt = TTY::Prompt.new
+            menu_options = prompt.select("What would you like to do next", ["Purchase Ticket", "Start Over"])
+                if menu_options == "Purchase Ticket"
+                    puts "Here is your ticket! Excited yet?!"
+                    your_ticket = Ticket.create(theater_id: Theater.all.first.id, movie_id: ticket_choice.id, guest_id: $guest.id) 
+                    puts your_ticket
+                    puts "Please save this id for your confirmation number."
+                    puts "You're going to see a #{ticket_choice.genre.genre} movie, #{your_ticket.movie.title}, @ #{ticket_choice.showtime} "
+                   
+                else
+                    welcome
         end
     end
 end
 
-def search
-    movies = Movie.all.map {|movie| movie.title}
-    prompt = TTY::Prompt.new
-    movie_choice = prompt.select("What would you like to see?", movies)
-    select_movie = Movie.find_by(title: movie_choice)
-    puts "You selected #{movie_choice}!!! We love that one!"
-    menu_options = prompt.select("What would you like to do next", ["Purchase Ticket", "Start Over"])
-        if menu_options == "Purchase Ticket"
-            puts "Here is your ticket! Excited yet?!"
-            your_ticket = Ticket.create(theater_id: Theater.all.first.id, movie_id: select_movie.id, guest_id: Guest.all.first.id) 
-            puts your_ticket
-            puts "You're going to see #{your_ticket.movie.title} at #{your_ticket.theater.name}. "
-        else 
-            welcome
-            login
-            search
-            thanks
-        end
-    end
 
+
+
+
+####how to save rat
     def thanks
         puts "Thank you for your purchase! Commando Theater is b.y.o.b so feel free and WELCOME to bring your own refreshments. We cant wait to watch a movie with you!"
         prompt = TTY::Prompt.new
-        thanks_choice = prompt.select("What would you like to do?", ["Rate Your Commando Expereince", "Start Over"])
-            if thanks_choice == "Rate Your Commando Experience"
-                rate= prompt.select("Give us a rating...", ["1", "2", "3", "4", "5"])
-                ###for thursday figure out why options not showing up
-                if rate == "1"
-                    puts "Thank you for your feedback."
-                end
-            end
+        thanks_choice = prompt.ask("Please leave us a rating from 1-5")
+        puts "Thank you for your feedback!"
         end
 
-
-
-                
-
-
-
-
-
-# def signup
-#     guests = Guest.all.map {|guest| guest.name}
-#     prompt = TTY::Prompt.new
-#     signup_choice = prompt.select("create username?", ["Login", "Sign Up", "continue as a guest"])
-#     if (signup_choice == "Signup")
-#         current_username = prompt.ask("What is your username?")
-#         current_password = prompt.ask("What is your password?")
-#         if guests.include?(current_username) && Guest.all.find_by(password: current_password)
-#             puts "Enjoy you movie!"
-#         else 
-#             puts "Incorrect username or password"
-#             exit!
-#         end
-#     end
-# end
 
 
 welcome
 login
 search
 thanks
-
