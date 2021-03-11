92a100a624ade6e400986653541531d91501bc3c
hadi <hadi_960@hotmail.com>
2021-03-11 10:11:36 -0600

    maerging

--- bin/program.rb
+++ bin/program.rb
@@ -7,21 +7,44 @@ puts "Welcome to"
 puts title.asciify("Command Theater")
 end
 
-def login
-    guests = Guest.all.map {|guest| guest.name}
+def signup
+    # guests = Guest.all.map {|guest| guest.name}
     prompt = TTY::Prompt.new
-    login_choice = prompt.select("What would you like to do?", ["Login", "Sign Up", "continue as a guest"])
-    if (login_choice == "Login")
-        current_username = prompt.ask("What is your username?")
-        current_password = prompt.ask("What is your password?")
-        if guests.include?(current_username) && Guest.all.find_by(password: current_password)
-            puts "Enjoy you movie!"
-        else 
-            puts "Incorrect username or password"
-            exit!
+    # signup_choice = prompt.collect do
+        g = prompt.ask('Create your username.')
+        $guest= Guest.create(name: g, password: 123)
+            # if Guest.all.map {|guest| guest.name}.include?(g)
+                # puts "username you provided has been used already!"
+                
+            end
+    
+
+def login
+    # guests = Guest.all.map {|guest| guest.name}
+    prompt = TTY::Prompt.new
+    login_choice = prompt.select("What would you like to do?", ["Signup"])
+    # if (login_choice == "Login")
+    #     current_username = prompt.ask("What is your username?")
+    #     current_password = prompt.ask("What is your password?")
+    #     if guests.include?(current_username) && Guest.all.find_by(password: current_password)
+    #         puts "Enjoy you movie!"
+        if (login_choice == "Signup")
+            prompt = TTY::Prompt.new
+            # signup_choice = prompt.collect do
+                new_username = prompt.ask('Create your username.')
+                new_password = prompt.ask("What is your password?")
+                $guest= Guest.create(name: new_username, password: new_password)
+                    # if Guest.all.map {|guest| guest.name}.include?(g)
+                        # puts "username you provided has been used already!"
+        
+        # else 
+        #     puts "Incorrect username or password"
+            # exit!
         end
     end
-end
+
+
+
 
 def search
     movies = Movie.all.map {|movie| movie.title}
@@ -32,7 +55,7 @@ def search
     menu_options = prompt.select("What would you like to do next", ["Purchase Ticket", "Start Over"])
         if menu_options == "Purchase Ticket"
             puts "Here is your ticket! Excited yet?!"
-            your_ticket = Ticket.create(theater_id: Theater.all.first.id, movie_id: select_movie.id, guest_id: Guest.all.first.id) 
+            your_ticket = Ticket.create(theater_id: Theater.all.first.id, movie_id: select_movie.id, guest_id: $guest.id) 
             puts your_ticket
             puts "You're going to see #{your_ticket.movie.title} at #{your_ticket.theater.name}. "
         else 
@@ -54,24 +77,6 @@ def search
                 
 
 
-
-
-
-# def signup
-#     guests = Guest.all.map {|guest| guest.name}
-#     prompt = TTY::Prompt.new
-#     signup_choice = prompt.select("create username?", ["Login", "Sign Up", "continue as a guest"])
-#     if (signup_choice == "Signup")
-#         current_username = prompt.ask("What is your username?")
-#         current_password = prompt.ask("What is your password?")
-#         if guests.include?(current_username) && Guest.all.find_by(password: current_password)
-#             puts "Enjoy you movie!"
-#         else 
-#             puts "Incorrect username or password"
-#             exit!
-#         end
-#     end
-# end
 
 
 welcome
