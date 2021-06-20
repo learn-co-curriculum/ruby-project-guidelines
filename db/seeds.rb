80e6ba3aa9dc5d2e0d45358f26a6fbf048dba930
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-20 16:39:19 -0400

    added rounding to price of pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ puts "Creating pets..."
 SPECIES = ["Cat", "Dog", "Bird", "Lizard", "Frog"]
 
 20.times do 
-    Pet.create(nickname: Faker::FunnyName.name, species: SPECIES.sample, weight: rand(1..20), age: rand(1..20), alive: rand(0..1), years_in_captivity: rand(0..10), price: rand(1.5...100.0))
+    Pet.create(nickname: Faker::FunnyName.name, species: SPECIES.sample, weight: rand(1..20), age: rand(1..20), alive: rand(0..1), years_in_captivity: rand(0..10), price: rand(1.5...100.0).round(2))
 end
 
 puts "Done creating pets!"
