1f542f4630566981887239b69eea1b6bce6d1e9e
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-22 12:26:54 -0400

    updated seeds with adoptions and stores

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,8 @@ end
 
 puts "Creating employees..."
 
-4.times do 
-    Employee.create(name: Faker::FunnyName.name, years_experience: rand(1..5), full_time: rand(0..1), hours_scheduled: rand(0..40), age: rand(16..80), salary: rand(8000..40000))
+20.times do 
+    Employee.create(name: Faker::FunnyName.name, years_experience: rand(1..5), full_time: rand(0..1), hours_scheduled: rand(0..40), age: rand(16..80), salary: rand(8000..40000), store_id: Store.ids.sample)
 end
 
 puts "Done creating employees!"
@@ -32,18 +32,18 @@ puts "Creating pets..."
 
 SPECIES = ["Cat", "Dog", "Bird", "Lizard", "Frog"]
 
-20.times do 
-    Pet.create(nickname: Faker::FunnyName.name, species: SPECIES.sample, weight: rand(1..20), age: rand(1..20), alive: rand(0..1), years_in_captivity: rand(0..10), price: rand(1.5...100.0).round(2))
+40.times do 
+    Pet.create(nickname: Faker::FunnyName.name, species: SPECIES.sample, weight: rand(1.0..20.0).round(2), age: rand(1..20), alive: rand(0..1), years_in_captivity: rand(0..10), price: rand(1.5...100.0).round(2))
 end
 
 puts "Done creating pets!"
 
-# STORES
+# ADOPTIONS
 
-puts "Creating Stores.."
+puts "Creating adoptions.."
 
-3.times do 
-    Store.create(employee_id: Employee.ids.sample, pet_id: Pet.ids.sample, city: Faker::Address.city, name: Faker::FunnyName.name + "'s Pet Store")
+40.times do 
+    Adoption.create(employee_id: Employee.ids.sample, pet_id: Pet.ids.sample)
 end 
 
-puts "Done creating stores!"
+puts "Done creating adoptions!"
