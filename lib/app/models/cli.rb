58f9da896e082606898e72e949bd8da9e13ad16d
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-23 10:16:07 -0400

    about to merger

---
+++ lib/app/models/cli.rb
@@ -0,0 +1,37 @@
+require "tty-prompt"
+require "tty-font"
+class CLI
+    @@prompt = TTY::Prompt.new
+    @@pastel = Pastel.new
+    @@font = TTY::Font.new(:standard)
+
+
+    def self.title_screen
+        system('clear')
+        self.title
+        self.art
+
+    end
+
+    def self.title
+        puts @@pastel.blue(@@font.write("Pet Store Simulator", letter_spacing: 2))
+    end
+
+    def self.art
+        art = <<-HRD
+                      /^--^\     /^--^\     /^--^\
+                      \____/     \____/     \____/
+                     /      \   /      \   /      \
+                    |        | |        | |        |
+                     \__  __/   \__  __/   \__  __/
+|^|^|^|^|^|^|^|^|^|^|^|^\ \^|^|^|^/ /^|^|^|^|^\ \^|^|^|^|^|^|^|^|^|^|^|^|
+| | | | | | | | | | | | |\ \| | |/ /| | | | | | \ \ | | | | | | | | | | |
+########################/ /######\ \###########/ /#######################
+| | | | | | | | | | | | \/| | | | \/| | | | | |\/ | | | | | | | | | | | |
+|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|
+    HRD
+        puts art
+    end
+
+
+end
